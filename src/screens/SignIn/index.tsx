693243a99bbe8b601634fe5e423bfb5df6dea254
Samuel Alves <samuellmsl05@gmail.com>
2021-11-03 15:43:09 -0300

    :sparkles: Validação do formulario de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,8 +3,11 @@ import {
     StatusBar,
     KeyboardAvoidingView,
     TouchableWithoutFeedback,
-    Keyboard
+    Keyboard,
+    Alert
 } from 'react-native';
+
+import * as Yup from 'yup';
 
 import { useTheme } from 'styled-components';
 
@@ -26,6 +29,29 @@ export function SignIn() {
     const [password, setPassword] = useState('');
 
     const theme = useTheme();
+
+    async function handleSignIn() {
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string()
+                    .required('A senha é obrigatória')
+            });
+
+            await schema.validate({ email, password });
+        } catch (error) {
+            if (error instanceof Yup.ValidationError) {
+                Alert.alert('Opa', error.message);
+            } else {
+                Alert.alert(
+                    'Erro na autenticação',
+                    'Ocorreu um erro ao fazer login, verifique as credenciais'
+                );
+            }
+        }
+    }
 
     return (
 
@@ -75,7 +101,7 @@ export function SignIn() {
 
                         <Button
                             title="Login"
-                            onPress={() => { }}
+                            onPress={handleSignIn}
                             enabled={false}
                             loading={false}
                         />
