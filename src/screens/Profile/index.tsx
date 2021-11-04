db08234578e8feeffacd92cb62bc61ab21fd15b8
Samuel Alves <samuellmsl05@gmail.com>
2021-11-04 18:05:52 -0300

    :construction: Edição de perfil e seleção de avatar

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,15 @@
-import { useNavigation } from '@react-navigation/core';
 import React, { useState } from 'react';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
 import { Feather } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 import { BackButton } from '../../components/BackButton';
+import { Input } from '../../components/Input';
+import { PasswordInput } from '../../components/PasswordInput';
+import { useAuth } from '../../hooks/auth';
 
 import {
     Container,
@@ -17,11 +23,18 @@ import {
     Content,
     Options,
     Option,
-    OptionTitle
+    OptionTitle,
+    Section
 } from './styles';
 
+
 export function Profile() {
+    const { user } = useAuth();
+
     const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
+    const [avatar, setAvatar] = useState(user.avatar);
+    const [name, setName] = useState(user.name);
+    const [driveLicense, setDriverLicense] = useState(user.driver_license);
 
     const theme = useTheme();
 
@@ -39,50 +52,120 @@ export function Profile() {
         setOption(optionSelected);
     }
 
-    return (
-        <Container>
-            <Header>
-                <HeaderTop>
-                    <BackButton color={theme.colors.shape} onPress={handleBack} />
-                    <HeaderTitle>
-                        Editar Perfil
-                    </HeaderTitle>
-                    <LogoutButton onPress={handleSignOut} >
-                        <Feather name="power" size={24} color={theme.colors.shape} />
-                    </LogoutButton>
-                </HeaderTop>
-                <PhotoContainer>
-                    <Photo source={{ uri: 'https://github.com/samuellalvs.png' }} />
-                    <PhotoButton onPress={() => { }}>
-                        <Feather
-                            name="camera"
-                            size={24}
-                            color={theme.colors.shape}
-                        />
+    async function handleSelectAvatar() {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1
+        });
 
-                    </PhotoButton>
-                </PhotoContainer>
-            </Header>
-            <Content>
-                <Options>
-                    <Option
-                        active={option === 'dataEdit'}
-                        onPress={() => handleOptionChange('dataEdit')}
-                    >
-                        <OptionTitle active={option === 'dataEdit'}>
-                            Dados
-                        </OptionTitle>
-                    </Option>
-                    <Option
-                        active={option === 'passwordEdit'}
-                        onPress={() => handleOptionChange('passwordEdit')}
-                    >
-                        <OptionTitle active={option === 'passwordEdit'}>
-                            Trocar senha
-                        </OptionTitle>
-                    </Option>
-                </Options>
-            </Content>
-        </Container>
+        if (result.cancelled) {
+            return;
+        }
+
+        if (result.uri) {
+            setAvatar(result.uri);
+        }
+    }
+
+    return (
+        <KeyboardAvoidingView behavior="position" enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <Header>
+                        <HeaderTop>
+                            <BackButton color={theme.colors.shape} onPress={handleBack} />
+                            <HeaderTitle>
+                                Editar Perfil
+                            </HeaderTitle>
+                            <LogoutButton onPress={handleSignOut} >
+                                <Feather name="power" size={24} color={theme.colors.shape} />
+                            </LogoutButton>
+                        </HeaderTop>
+                        <PhotoContainer>
+                            {
+                                !!avatar && <Photo source={{ uri: avatar }} />
+                            }
+
+                            <PhotoButton onPress={handleSelectAvatar}>
+                                <Feather
+                                    name="camera"
+                                    size={24}
+                                    color={theme.colors.shape}
+                                />
+
+                            </PhotoButton>
+                        </PhotoContainer>
+                    </Header>
+                    <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+                        <Options>
+                            <Option
+                                active={option === 'dataEdit'}
+                                onPress={() => handleOptionChange('dataEdit')}
+                            >
+                                <OptionTitle active={option === 'dataEdit'}>
+                                    Dados
+                                </OptionTitle>
+                            </Option>
+                            <Option
+                                active={option === 'passwordEdit'}
+                                onPress={() => handleOptionChange('passwordEdit')}
+                            >
+                                <OptionTitle active={option === 'passwordEdit'}>
+                                    Trocar senha
+                                </OptionTitle>
+                            </Option>
+                        </Options>
+
+                        {
+                            option === 'dataEdit'
+                                ?
+                                <Section >
+                                    <Input
+                                        iconName="user"
+                                        placeholder="Nome"
+                                        autoCorrect={false}
+                                        defaultValue={user.name}
+                                        onChangeText={setName}
+                                    />
+                                    <Input
+                                        iconName="mail"
+                                        editable={false}
+                                        defaultValue={user.email}
+                                    />
+                                    <Input
+                                        iconName="credit-card"
+                                        placeholder="CNH"
+                                        keyboardType="numeric"
+                                        defaultValue={user.driver_license}
+                                        onChangeText={setDriverLicense}
+
+                                    />
+                                </Section>
+                                :
+                                <Section >
+                                    <PasswordInput
+                                        iconName="lock"
+                                        placeholder="Senha atual"
+                                    />
+                                    <PasswordInput
+                                        iconName="lock"
+                                        placeholder="Nova senha"
+                                    />
+                                    <PasswordInput
+                                        iconName="lock"
+                                        placeholder="Repetir senha"
+                                    />
+                                </Section>
+                        }
+
+
+
+                    </Content>
+                </Container >
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+
     );
 }
